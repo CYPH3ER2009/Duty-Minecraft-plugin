136917e6936b975098e5bffb44ac405168142dc3
CYPH3ER2009 <maniredx.2000@gmail.com>
2025-06-28 14:47:31 +0330

    Update DutyListCommand.java

--- DutyProject/src/main/java/me/cyph3er/dutymanager/commands/DutyListCommand.java
+++ DutyProject/src/main/java/me/cyph3er/dutymanager/commands/DutyListCommand.java
@@ -1,21 +1,26 @@
 package me.cyph3er.dutymanager.commands;
 
-import me.cyph3er.dutymanager.gui.DutyListGUI;
+import me.cyph3er.dutymanager.DutyManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class DutyListCommand implements CommandExecutor {
+
+    private final DutyManager plugin;
+
+    public DutyListCommand(DutyManager plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
         if (!(sender instanceof Player player)) {
-            sender.sendMessage("§c only staff can use this");
             return true;
         }
 
-        new DutyListGUI().open(player);
+        plugin.getDutyListGUI().open(player);
         return true;
     }
 }
